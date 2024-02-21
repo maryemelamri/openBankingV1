8d0a50db41abfbafd1f95e0bab97714a48adb047
maryem elamri <meriammeri.mm@gmail.com>
2024-02-21 09:48:02 +0100

    first

---
+++ src/test/java/elamri/effyis/openbanking/OpenbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package elamri.effyis.openbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
